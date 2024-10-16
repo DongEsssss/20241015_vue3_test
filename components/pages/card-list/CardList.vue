6a369d77fba92a1c5b5da94f743fb82111e2ab8e
임동균 <70128434+DongEsssss@users.noreply.github.com>
2024-10-16 22:36:15 +0900

    shadow

--- components/pages/card-list/CardList.vue
+++ components/pages/card-list/CardList.vue
@@ -3,7 +3,7 @@
   <section>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-20">
       <Card v-for="(card, index) in cards" :key="index" :title="card.title" :description="card.description"
-        :category="card.category" :images="card.images" />
+        :category="card.category" :images="card.images" class="shadow-xl" />
     </div>
   </section>
 </template>
